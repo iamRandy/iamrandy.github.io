7e553391a357b5dc8c064a31fc7193c4d299382a
Randy Thai <randythai360@gmail.com>
2023-06-30 00:27:36 -0500

    collapse testing

--- javascript/animationConfig.js
+++ javascript/animationConfig.js
@@ -1,6 +1,19 @@
 var exbutton = document.getElementById('openDescriptionButton');
 var descriptionContainer = document.getElementById('descriptionContainer');
 
+var collapseButton = document.getElementById('collapseButton');
+var visualUnderlay = document.getElementById('projectVisUnderlay');
+var content = document.getElementById('content1');
+
+collapseButton.addEventListener('click', function(){
+
+    collapseButton.classList.toggle('move');
+    visualUnderlay.classList.toggle('move');
+    content.classList.toggle('expand');
+
+})
+
+
 exbutton.addEventListener('click', function() {
     exbutton.classList.toggle('stop-animation');
     exbutton.classList.toggle('clicked');
