661f6b8e17ae2b57fe6cfe59ee0a1f52a430fdbe
Randy Thai <randythai360@gmail.com>
2024-08-11 16:23:01 -0500

    mobile friendly attempt

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ function App() {
       <section id="home">
         <div id="header" className="flex justify-center bg-center bg-cover bg-local">
           
+
           <div id="profileContainer" className="absolute top-10 right-40">
             <img id="pfp" src={profile} alt="pfp" className="relative" />
           </div>
           <div id="profile_description_container">
             <div id="profile_description_text" className="profile_description_text">
-              <p>An artist and a developer. Learning everywhere he goes, he tries to utilize his skills to bring smiles on peoples' faces everyday :)</p>
+              <p>An artist and a developer, learning everywhere he goes. He strives to utilize his skills to bring smiles to people’s faces every day :)</p>
             </div>
           </div>
+
           
         </div>
       </section>
