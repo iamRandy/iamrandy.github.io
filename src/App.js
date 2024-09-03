1079494aa04d77ed265d42f993cd9ffc622fb830
Randy Thai <randythai360@gmail.com>
2024-09-03 10:32:00 -0500

    focus on images

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ function App() {
     <div id="MAIN" className="min-h-80 flex flex-col">
       <div id="homeButton" className="linkButton home">
         <span id="homeButton" className="linkIcon" title="home">
-          <svg id="homeButton" xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="white" class="bi bi-house" viewBox="0 0 16 16">
+          <svg id="homeButton" xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="white" className="bi bi-house" viewBox="0 0 16 16">
             <path id="homeButton" d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
           </svg>
         </span>
--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import nullimg from '../imgs/null.jpeg';
 
 function ProjectComponent({name = "no name", 
@@ -7,11 +8,21 @@ function ProjectComponent({name = "no name",
     titles = [null, null, null],
     image_shapes = ["", "", ""]
 }) {
+    useEffect(() => {
+        const target_imgs = document.querySelectorAll(".project_images");
+
+        target_imgs.forEach((e) => {
+            e.addEventListener('click', () => {
+                e.classList.contains("focus") ? e.classList.remove("focus") : e.classList.add("focus");
+            });
+        });
+    });
+
     return(
         <section id="remora">
             <div className="project_hero">
                 <h1>{name}</h1>
-                <img id="remora_logo_image" title="remora logo" className="project_hero_image" src={header_image} />
+                <img id="project header image" alt="remora logo" title="remora logo" className="project_hero_image" src={header_image} />
             </div>
 
             <div className="project_display">
@@ -19,9 +30,9 @@ function ProjectComponent({name = "no name",
                     {description}
                 </div>
                 <div className="project_supporting_images">
-                    <img title={titles[0]} id="project_img1" className={`project_images ${image_shapes[0]}`} src={supporting_images[0]}></img>
-                    <img title={titles[1]} id="project_img2" className={`project_images ${image_shapes[1]}`} src={supporting_images[1]}></img>
-                    <img title={titles[2]} id="project_img3" className={`project_images ${image_shapes[2]}`} src={supporting_images[2]}></img>
+                    <img alt={titles[0]} title={titles[0]} id="project_img1" className={`project_images ${image_shapes[0]}`} src={supporting_images[0]}></img>
+                    <img alt={titles[1]} title={titles[1]} id="project_img2" className={`project_images ${image_shapes[1]}`} src={supporting_images[1]}></img>
+                    <img alt={titles[2]} title={titles[2]} id="project_img3" className={`project_images ${image_shapes[2]}`} src={supporting_images[2]}></img>
                 </div>
             </div>
         </section>
